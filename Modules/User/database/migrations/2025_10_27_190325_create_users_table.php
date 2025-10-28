b2c387cec7d05c3927d4edeb3909ae61db2b29b9
davismarciel <davisamarciel@gmail.com>
2025-10-28 00:46:10 -0300

    feat: add soft deletes to users table migration

--- Modules/User/database/migrations/2025_10_27_190325_create_users_table.php
+++ Modules/User/database/migrations/2025_10_27_190325_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('avatar')->nullable();
             $table->string('provider')->default('google');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
