7636abc32b8e725d77d977ee0ccdc1f8852413d3
davismarciel <davisamarciel@gmail.com>
2025-10-28 23:03:11 -0300

    feat: add test endpoint to user routes
    - Add GET /api/v1/test endpoint for API health check
    - Provides server and client IP information for debugging

--- Modules/User/routes/api.php
+++ Modules/User/routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\User\Http\Controllers\UserController;
 
 Route::prefix('v1')->group(function () {
+    Route::get('test', [UserController::class, 'test']);
     Route::apiResource('users', UserController::class);
 });
