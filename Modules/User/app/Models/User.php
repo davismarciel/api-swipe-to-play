0f1b9c1badc06fb047e8478ab5114b4bb0a7acfc
davismarciel <davisamarciel@gmail.com>
2025-10-28 23:03:08 -0300

    feat: implement JWT authentication in User model
    - Add JWTSubject interface implementation
    - Implement getJWTIdentifier method
    - Add getJWTCustomClaims method for JWT token customization

--- Modules/User/app/Models/User.php
+++ Modules/User/app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable, SoftDeletes;
 
@@ -29,5 +30,15 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
         ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
